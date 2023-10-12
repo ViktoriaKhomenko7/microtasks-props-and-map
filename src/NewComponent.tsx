d140da526fbe948b6934d7ac894243edc34ac0ea
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-12 14:05:22 +0300

    render table of topCarsArray using map

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
 
-type NewComponentType = {
-    students: StudentsType[]
+type NewComponentPropsType = {
+    topCars: TopCarsType[]
 }
 
-type StudentsType = {
-    id: number
-    name: string
-    age: number
+type TopCarsType = {
+    manufacturer: string
+    model: string
 }
 
-export const NewComponent = (props: NewComponentType) => {
+export const NewComponent = (props: NewComponentPropsType) => {
     return (
-           <ul>
-               {props.students.map((objectFromStudentsArray)=>{
-                   return (
-                       <li key={objectFromStudentsArray.id}>
-                           <span>{objectFromStudentsArray.name}</span>
-                           <span> age: {objectFromStudentsArray.age}</span>
-                       </li>
-                   )
-               })}
-           </ul>
-    );
-};
+        <table>
+            <thead>
+            <tr>
+                <td>Марка</td>
+                <td>Модель</td>
+            </tr>
+            </thead>
+            <tbody>
+            {props.topCars.map((objectOfTopCarsArray) => {
+                return (
+                    <tr key={objectOfTopCarsArray.manufacturer}>
+                        <td>{objectOfTopCarsArray.manufacturer}</td>
+                        <td>{objectOfTopCarsArray.model}</td>
+                    </tr>
+                )
+            })}
+            </tbody>
+        </table>
+    )
+}
+
+
+// type NewComponentType = {
+//     students: StudentsType[]
+// }
+//
+// type StudentsType = {
+//     id: number
+//     name: string
+//     age: number
+// }
+//
+// export const NewComponent = (props: NewComponentType) => {
+//     return (
+//            <ul>
+//                {props.students.map((objectFromStudentsArray)=>{
+//                    return (
+//                        <li key={objectFromStudentsArray.id}>
+//                            <span>{objectFromStudentsArray.name}</span>
+//                            <span> age: {objectFromStudentsArray.age}</span>
+//                        </li>
+//                    )
+//                })}
+//            </ul>
+//     );
+// };
